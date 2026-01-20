1ba0ee42e791017d24404febb8b969d4d2e33462
Kabir Patel <119153121+kabirpatel1203@users.noreply.github.com>
2026-01-20 03:50:04 +0000

    fix the login page

--- frontend/src/component/Auth/Signin/signin.js
+++ frontend/src/component/Auth/Signin/signin.js
@@ -75,6 +75,9 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+        <Typography component="h1" variant="h3" style={{ fontWeight: 'bold', marginBottom: '20px' }}>
+          Safal Marketing
+        </Typography>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
@@ -114,18 +117,6 @@ export default function SignIn() {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href='/password/forgot' variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            {/* <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid> */}
-          </Grid>
         </form>
       </div>
       <Box mt={8}>
